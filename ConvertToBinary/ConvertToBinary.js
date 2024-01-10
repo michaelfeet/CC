47db836306305a4820aee05dfd3d1ebe079b050a
michaelfeet <michael.a.feet@gmail.com>
2024-01-10 10:35:42 -0500

    convert to binary

---
+++ ConvertToBinary/ConvertToBinary.js
@@ -0,0 +1,12 @@
+// Task Overview
+// Given a non-negative integer b, write a function which returns an integer d such that the binary representation of b is the same as the decimal representation of d.
+
+// Examples:
+
+// 1  ->    1  (because  1 is    0b1 in binary)
+// 5  ->  101  (because  5 is  0b101 in binary)
+// 11 -> 1011  (because 11 is 0b1011 in binary)
+
+const toBinary = (n) => Number(n.toString(2))
+
+console.log(toBinary(5))
