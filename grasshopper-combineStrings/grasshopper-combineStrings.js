e691a709ffc1354775f299fe3e343fd19956e41e
michaelfeet <michael.a.feet@gmail.com>
2024-01-20 07:55:47 -0500

    grasshopper combine strings

---
+++ grasshopper-combineStrings/grasshopper-combineStrings.js
@@ -0,0 +1,11 @@
+// Description:
+// Combine strings function
+// Create a function named combineNames/combine_names/CombineNames that accepts two parameters (first and last name). The function should return the full name.
+
+// Example:
+
+// With "James" as the first name and "Stevens" as the last name should return "James Stevens"
+
+const combineNames = (first, last) => `${first} ${last}`
+
+console.log(combineNames('michael', 'feet'))
